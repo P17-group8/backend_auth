cd2710c9cc02faaf1e8a1df156596a114499f08c
efrainlav <efrainlav@gmail.com>
2021-11-29 16:51:20 -0500

    Cambio del user_operador por username

--- app_operator/serializers/operadorSerializer.py
+++ app_operator/serializers/operadorSerializer.py
@@ -1,20 +1,20 @@
-from rest_framework import serializers
-from app_operator.models.operador import Operador
-from rest_framework.relations import PrimaryKeyRelatedField
+from app_operator.models.operador   import Operador
+from rest_framework                 import serializers
+#from rest_framework.relations import PrimaryKeyRelatedField
 
 
 class OperadorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Operador
-        fields = ['id','nombre_operador','apellido_operador','user_operador','password','identificacion','email']
+        fields = ['id','nombre_operador','apellido_operador','username','password','identificacion','email']
     
     def to_representation(self, obj):
         operador = Operador.objects.get(id=obj.id)
         return {
-            'id':operador.id,
-            'nombre':operador.nombre_operador,
-            'apellido':operador.apellido_operador,
-            'user_operador':operador.user_operador,
-            'identificacion':operador.identificacion,
-            'email':operador.email
+            'id'                :operador.id,
+            'nombre'            :operador.nombre_operador,
+            'apellido'          :operador.apellido_operador,
+            'username'          :operador.username,
+            'identificacion'    :operador.identificacion,
+            'email'             :operador.email
         }   
