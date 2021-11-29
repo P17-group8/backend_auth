15424d0c0703ff2e8164c9157acd8a672f981094
efrainlav <efrainlav@gmail.com>
2021-11-29 16:53:59 -0500

    Modificacion y creacion de vistas usando el modelo del username

--- app_operator/views/operadorCreateView.py
+++ app_operator/views/operadorCreateView.py
@@ -1,11 +1,18 @@
-from rest_framework import status, views
-from rest_framework.response import Response
-from app_operator.serializers import OperadorSerializer
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework                                 import status, views
+from rest_framework.response                        import Response
+from rest_framework_simplejwt.serializers           import TokenObtainPairSerializer
+#ELM - Creo que este era el error, al importar el serializer faltaba adicionar el .serializers.operadorSerializer 
+from app_operator.serializers.operadorSerializer    import OperadorSerializer
+
+class OperadorWelcomeView(views.APIView):
+    def get(self, request, *args, **kwargs):
+        message = {"Detail" : "Welcome to AuthMS-Operador."}
+        return Response(message, status=status.HTTP_200_OK)
+
 
 class OperadorCreateView(views.APIView):
     def post(self, request, *args, **kwargs):
-        print('a')
+   #     print('a')
         serializer = OperadorSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
