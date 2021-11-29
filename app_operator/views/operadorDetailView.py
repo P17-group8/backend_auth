81d15f0b8f0a0a5e398fcf136aa8739fb70c70d5
efrainlav <efrainlav@gmail.com>
2021-11-29 16:54:23 -0500

    Modificacion y creacion de vistas usando el modelo del username, y se agrego el CRUD

--- app_operator/views/operadorDetailView.py
+++ app_operator/views/operadorDetailView.py
@@ -1,15 +1,25 @@
-from rest_framework                              import generics, status
-from rest_framework.response                     import Response
-from app_operator.models.operador                import Operador
-from app_operator.serializers.operadorSerializer import OperadorSerializer
+from rest_framework                                 import generics#, status
 
-class OperadorDetailView(generics.RetrieveAPIView, generics.UpdateAPIView, generics.DestroyAPIView):
+from app_operator.models.operador                   import Operador
+from app_operator.serializers.operadorSerializer    import OperadorSerializer
+#
+
+class OperadorDetailView(generics.RetrieveAPIView):
     queryset           = Operador.objects.all()
     serializer_class   = OperadorSerializer
-
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
-    def put(self, request, *args, **kwargs):
+
+
+class OperadorUpdateView(generics.UpdateAPIView):
+    queryset           = Operador.objects.all()
+    serializer_class   = OperadorSerializer
+    def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
+
+
+class OperadorDeleteView(generics.DestroyAPIView):
+    queryset           = Operador.objects.all()
+    serializer_class   = OperadorSerializer
     def delete(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
