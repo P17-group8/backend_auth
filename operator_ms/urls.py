b2d4bb8089fc273b817d9802edf3cf71e8f8e903
efrainlav <efrainlav@gmail.com>
2021-11-29 16:59:37 -0500

    actualizacion rutas-crud

--- operator_ms/urls.py
+++ operator_ms/urls.py
@@ -1,15 +1,16 @@
-from django.contrib import admin
-from django.urls import path
-from app_operator import views 
-from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView, TokenVerifyView)
-
-from app_operator.views import VerifyTokenView
+from django.contrib                 import admin
+from django.urls                    import path
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+from app_operator                   import views 
 
 urlpatterns = [
-    path('admin/',             admin.site.urls),
-    path('login/',             TokenObtainPairView.as_view()),
-    path('refresh/',           TokenRefreshView.as_view()),
-    path('verifyToken/',       VerifyTokenView.as_view(), name='token_verify'),
-    path('operador/create/',   views.OperadorCreateView.as_view()),
-    path('operador/<int:pk>/', views.OperadorDetailView.as_view()),
+        path('',                          views.OperadorWelcomeView.as_view()),
+        path('admin/',                    admin.site.urls),
+        path('login/',                    TokenObtainPairView.as_view()),
+        path('refresh/',                  TokenRefreshView.as_view()),
+        path('verifyToken/',              views.VerifyTokenView.as_view()),
+        path('operador/',                 views.OperadorCreateView.as_view()),
+        path('operador/<int:pk>/',        views.OperadorDetailView.as_view()),
+        path('operador/update/<int:pk>/', views.OperadorUpdateView.as_view()),
+        path('operador/delete/<int:pk>/', views.OperadorDeleteView.as_view()),
 ]
